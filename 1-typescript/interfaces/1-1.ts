59ae1c737f31259c84b235e00db03acdcba42529
Tae-hyeon Kim <hypspacecraft@gmail.com>
2024-09-23 20:14:15 +0900

    [1-typescript] chore: implement interface for dictionary patterns

--- 1-typescript/interfaces/1-1.ts
+++ 1-typescript/interfaces/1-1.ts
@@ -1,3 +1,6 @@
+// TS 인터페이스 정의
+
+// 변수
 interface User {
   name: string
   age: number
@@ -8,17 +11,33 @@ const wang: User = {
   age: 21,
 }
 
+// - 함수 구조
 interface GetUsername {
   (user: User): string
 }
 
+// - 함수 인자
 function getUsername(user: User) {
   return user.name
 }
 
+// - 인덱싱 방식
 interface stringArray {
   [index: number]: string
 }
 
 const arr: stringArray = ['A', 'B', 'C']
-arr[0] = 10
+// arr[0] = 10 // Error!
+
+// : 딕셔너리 패턴
+interface StringRegexDictionary {
+  [key: string]: RegExp
+}
+
+const obj: StringRegexDictionary = {
+  // sth: /abc/,
+  cssFile: /\.css$/,
+  jsFile: /\.js$/,
+}
+
+// obj['cssFile'] = 'a' // Error!
